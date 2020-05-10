84a4dcc76bdd2566b1afb338b788d52c355f835a
shay <shayben838@gmail.com>
2020-05-10 21:51:01 +0300

    df

--- DB/config_db.js
+++ DB/config_db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'shayben838',
-    database: 'meet_artist'
+    host: 'us-cdbr-east-06.cleardb.net',
+    user: 'bf41c42b19f6ea',
+    password: '9147af7f',
+    database: 'heroku_65ed7343bb0cf12'
 });
 
 module.exports = connection;
