fc8a121594fd46d950408ef66cae122ab6f66b96
shay <shayben838@gmail.com>
2020-05-11 18:39:56 +0300

    df

--- routes/cities.js
+++ routes/cities.js
@@ -4,9 +4,14 @@ const router = express.Router()
 const get_cities = require("../DB/api/cities_and_countries/get_cities")
 
 router.get("/",function(req,res){
-    get_cities().
+    try{
+        get_cities().
     then(result=> res.status(200).json({result}) )
     .catch(error=>res.status(200).json({error:error}))
+    }
+    catch (error){
+        res.status(404).json({error})
+    }
 })
 
 
