0ce2edc1a7e117ed5824026fb9e1950c23614b32
shay <shayben838@gmail.com>
2020-05-11 22:14:34 +0300

    df

--- routes/cities.js
+++ routes/cities.js
@@ -4,15 +4,9 @@ const router = express.Router()
 const get_cities = require("../DB/api/cities_and_countries/get_cities")
 
 router.get("/",function(req,res){
-    // try{
-    // get_cities().
-    // then(result=> res.status(200).json({result}) )
-    // .catch(error=>res.status(201).json({error:error}))
-    // }
-    // catch (error){
-    //     res.status(404).json({error})
-    // }
-    res.status(200).json({test:'if this acceft in the client the cors problem couse because mysql connection!'})
+    get_cities().
+    then(result=> res.status(200).json({result}) )
+    .catch(error=>res.status(201).json({error:error}))
 })
 
 
